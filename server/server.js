ae924a38e503c315e70dd6debeef2b3110410076
Adisak-KS <Adisak.General@gmail.com>
2024-11-03 13:39:04 +0700

    Routing

--- server/server.js
+++ server/server.js
@@ -1,9 +1,30 @@
 // Step 1 Import Express
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const {readdirSync} = require("fs");
+const cors = require("cors");
 
 
-//Step 2 Start Server
+// import Router
+// const authRouter = require("./routers/auth");
+// const userRouter = require("./routers/user");
+
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+// Step 3 Route
+readdirSync("./routers").map((item)=> app.use("/api", require(`./routers/${item}`)));
+// app.use('/api', authRouter);
+// app.use('/api', userRouter);
+
+
+// Step 2 Start Server
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 });
